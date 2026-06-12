5951e0069d42ab3dfd9d51df1b22ad50b447665f
jessica7241 <jg7896410@gmail.com>
2026-06-12 17:39:00 -0600

    Add files via upload

---
+++ Pages/Colaboradores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistemas.Solution.Pages
+{
+    public class ColaboradoresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Productos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistemas.Solution.Pages
+{
+    public class ProductosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
